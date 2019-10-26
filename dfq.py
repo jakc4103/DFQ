1d1bc751c0d0759cc9dded404d2b1499aa59623b
JackWei <jakc4103@gmail.com>
2019-10-26 10:26:25 +0800

    add dfq module

---
+++ dfq.py
@@ -0,0 +1,84 @@
+import torch
+import torch.nn as nn
+import copy
+from utils.utils import visualize_per_layer
+from PyTransformer.transformers.quantize import QConv2d
+
+def _layer_equalization(weight_first, weight_second, bias_first, s_range=(1e-8, 1e8), signed=False, eps=0):
+    num_group = 1
+    if weight_first.shape[0] != weight_second.shape[1]:
+        # group convolution
+        num_group = weight_first.shape[0] // weight_second.shape[1]
+	
+    group_channels_i = weight_first.shape[0] // num_group
+    group_channels_o = weight_second.shape[0] // num_group
+
+    # pdb.set_trace()
+    for g in range(num_group):
+        c_start_i = g * group_channels_i
+        c_end_i = (g+1) * group_channels_i
+        weight_first_group = weight_first[c_start_i:c_end_i] # shape [k, c, h, w]
+
+        c_start_o = g * group_channels_o
+        c_end_o = (g+1) * group_channels_o
+        weight_second_group = weight_second[c_start_o:c_end_o]
+
+        for ii in range(weight_second_group.shape[1]):
+            if signed:
+                range_1 = torch.max(torch.abs(weight_first_group[ii])) # signed
+                range_2 = torch.max(torch.abs(weight_second_group[:, ii])) # signed
+
+            else:
+                range_1 = torch.max(weight_first_group[ii]) - torch.min(weight_first_group[ii]) # unsigned
+                range_2 = torch.max(weight_second_group[:, ii]) - torch.min(weight_second_group[:, ii]) # unsigned
+            
+            # 1 / s = (1 / r1) * sqrt(r1 * r2)
+            s = (1 / (range_1 + eps)) * torch.sqrt(range_1 * range_2 + eps)
+            s = max(s_range[0], min(s_range[1], s))
+
+            weight_first[c_start_i + ii].mul_(s)
+
+            if bias_first is not None:
+                bias_first[c_start_i + ii].mul_(s)
+
+            weight_second[c_start_o:c_end_o, ii].mul_(1/s)
+
+    return weight_first, weight_second, bias_first
+
+
+def cross_layer_equalization(graph, relations, s_range=[1e-8, 1e8], converge_thres=1, signed=False, eps=0, visualize_state=False):
+    with torch.no_grad():
+        diff = 10
+        while diff > converge_thres:
+            state_prev = copy.deepcopy(graph)
+            for rr in relations:
+                layer_first, layer_second = rr.get_idxs()
+                
+                # 17-20, 36-39, 65-68, 104-107, 133-136, 162-164
+                # if '17' in layer_first or '36' in layer_first or '65' in layer_first or '104' in layer_first or '133' in layer_first or '162' in layer_first:
+                #     continue
+                
+                if visualize_state:
+                    visualize_per_layer(graph[layer_first].weight.detach(), 'Before equalization')
+
+                if graph[layer_first].bias is None: # add a fake bias term
+                    graph[layer_first].bias = nn.Parameter(data=torch.zeros((graph[layer_first].weight.size(0)), dtype=torch.float), requires_grad=False)
+                
+                # layer eualization
+                graph[layer_first].weight, graph[layer_second].weight, graph[layer_first].bias = \
+                    _layer_equalization(graph[layer_first].weight,\
+                                        graph[layer_second].weight,\
+                                        graph[layer_first].bias, s_range=s_range, signed=signed, eps=eps)
+                
+                if visualize_state:
+                    visualize_per_layer(graph[layer_first].weight.detach(), 'After equalization')
+
+            diff_tmp = 0
+            for layer_idx in graph:
+                if type(graph[layer_idx]) == QConv2d:
+                    diff_tmp += torch.mean(torch.abs(graph[layer_idx].weight - state_prev[layer_idx].weight))
+            
+            diff = min(diff, diff_tmp)
+            print('diff', diff)
+    
+    # return graph
--- main.py
+++ main.py
@@ -8,35 +8,71 @@ from torch.utils.data import DataLoader
 from dataset.pascal import VOCSegmentation
 from utils.metrics import Evaluator
 
+from utils.relation import create_relation
+from utils.quantize import QIdentity
+from dfq import cross_layer_equalization
 from PyTransformer.transformers.quantize import QConv2d, QuantMeasure
 from PyTransformer.transformers.torchTransformer import TorchTransformer
 
-class QIdentity(nn.Module):
-    def __init__(self):
-        super(QIdentity, self).__init__()
-
-    
-    def forward(self, x):
-        return x
 
 
-def switch_layers(model):
+def process_model(model):
     transformer = TorchTransformer()
 
     # transformer.register(nn.BatchNorm2d, QIdentity)
     # model = transformer.trans_layers(model, update=False)
-    old_dict = model.state_dict()
+    
+    # replace layers
     transformer.register(nn.ReLU6, nn.ReLU)
     transformer.register(nn.Conv2d, QConv2d)
     model = transformer.trans_layers(model)
 
-    new_dict = model.state_dict()
-
-    data = torch.ones((4, 3, 512, 512)).cuda()
+    # use cpu to process
+    model = model.cpu()
+    data = torch.ones((4, 3, 512, 512))#.cuda()
     # transformer.summary(model, data)
     # transformer.visualize(model, data, 'test.png', graph_size=120)
     transformer._build_graph(model, data)
-    print(transformer.log.getGraph())
+
+    graph = transformer.log.getGraph()
+    bottoms = transformer.log.getBottoms()
+    
+    with torch.no_grad():
+        # merge bn params into QConv2d
+        for layer_idx in graph:
+            # print(bottoms[layer_idx])
+            if bottoms[layer_idx] is None:
+                continue
+            for bot_idx in bottoms[layer_idx]:
+                if type(graph[layer_idx]) == nn.BatchNorm2d and type(graph[bot_idx]) == QConv2d:
+                    conv_weight = graph[bot_idx].weight
+                    bn_weight = graph[layer_idx].weight
+                    bn_var = graph[layer_idx].running_var
+                    bn_eps = graph[layer_idx].eps
+
+                    graph[bot_idx].weight.copy_(conv_weight.clone().mul(bn_weight.view(-1, 1, 1, 1) / torch.sqrt(bn_var.view(-1, 1, 1, 1) + bn_eps)))
+                    
+                    if graph[bot_idx].bias is None:
+                        graph[bot_idx].bias = nn.Parameter(data=torch.zeros((graph[bot_idx].weight.size(0)), dtype=torch.float), requires_grad=False)
+
+                    conv_bias = graph[bot_idx].bias
+                    bn_bias = graph[layer_idx].bias
+                    bn_mean = graph[layer_idx].running_mean
+
+                    graph[bot_idx].bias.copy_(conv_bias.clone().mul(bn_weight.view(-1) / torch.sqrt(bn_var.view(-1) + bn_eps)).add(bn_bias.view(-1) -\
+						 (bn_weight.view(-1) * bn_mean.view(-1)) / torch.sqrt(bn_var.view(-1) + bn_eps)))
+
+                    graph[layer_idx].weight.mul_(0).add_(1)
+                    graph[layer_idx].running_var.mul_(0).add_(1)
+                    graph[layer_idx].bias.mul_(0)
+                    graph[layer_idx].running_mean.mul_(0)
+                    graph[layer_idx].eps = 0
+
+                    break
+
+    res = create_relation(graph, bottoms)
+
+    cross_layer_equalization(graph, res, visualize_state=False)
 
     return model
 
@@ -60,8 +96,9 @@ def main():
     # load trained parameters
     state_dict = torch.load('modeling/deeplab-mobilenet.pth.tar')['state_dict']
     model.load_state_dict(state_dict)
-    model = switch_layers(model)
-    # inference_all(model)
+    model = process_model(model)
+    model = model.cuda()
+    inference_all(model)
     # return
 
 
