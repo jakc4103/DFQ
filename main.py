b8fba917f585aa2f432dceed30225e7977efc2d5
JackWei <jakc4103@gmail.com>
2019-11-01 14:32:33 +0800

    finished cross_layer_equalization. further test needed

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+import inspect
+import time
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,97 +13,144 @@ from dataset.pascal import VOCSegmentation
 from utils.metrics import Evaluator
 
 from utils.relation import create_relation
-from utils.quantize import QIdentity
 from dfq import cross_layer_equalization
-from PyTransformer.transformers.quantize import QConv2d, QuantMeasure
+from utils.layer_transform import merge_batchnorm, switch_layers, replace_op, restore_op
 from PyTransformer.transformers.torchTransformer import TorchTransformer
 
 
+def estimate_stats(data, num_epoch=10):
+    import copy
 
-def process_model(model):
+    model = DeepLab(sync_bn=False)
+    model.eval()
+    state_dict = torch.load('modeling/deeplab-mobilenet.pth.tar')['state_dict']
+    model.load_state_dict(state_dict)
+
     transformer = TorchTransformer()
 
-    # transformer.register(nn.BatchNorm2d, QIdentity)
-    # model = transformer.trans_layers(model, update=False)
+    model = switch_layers(model, transformer, data)
     
-    # replace layers
-    transformer.register(nn.ReLU6, nn.ReLU)
-    transformer.register(nn.Conv2d, QConv2d)
-    model = transformer.trans_layers(model)
+    model = model.cuda()
+
+    args = lambda: 0
+    args.base_size = 513
+    args.crop_size = 513
+    voc_val = VOCSegmentation(args, split='train')
+    dataloader = DataLoader(voc_val, batch_size=32, shuffle=True, num_workers=0)
+    model.train()
+
+    replace_op()
+    ss = time.time()
+    with torch.no_grad():
+        for epoch in range(num_epoch):
+            start = time.time()
+            for sample in dataloader:
+                image, _ = sample['image'].cuda(), sample['label'].cuda()
+
+                _ = model(image)
+
+            end = time.time()
+            print("epoch {}: {} sec.".format(epoch, end-start))
+    print('total time: {} sec'.format(time.time() - ss))
+    restore_op()
+
+    # load 'running_mean' and 'running_var' of batchnorm back from pre-trained parameters
+    state_dict_old = torch.load('modeling/deeplab-mobilenet.pth.tar')['state_dict']
+    bn_dict = {}
+    for key in state_dict_old:
+        if 'running' in key:
+            bn_dict[key] = state_dict_old[key]
+
+    state = model.state_dict()
+    state.update(bn_dict)
+    model.load_state_dict(state)
 
     # use cpu to process
     model = model.cpu()
-    data = torch.ones((4, 3, 512, 512))#.cuda()
-    # transformer.summary(model, data)
-    # transformer.visualize(model, data, 'test.png', graph_size=120)
-    transformer._build_graph(model, data)
+    
+    transformer._build_graph(model, data) # construt graph after all state_dict loaded
 
     graph = transformer.log.getGraph()
     bottoms = transformer.log.getBottoms()
-    
-    with torch.no_grad():
-        # merge bn params into QConv2d
-        for layer_idx in graph:
-            # print(bottoms[layer_idx])
-            if bottoms[layer_idx] is None:
-                continue
-            for bot_idx in bottoms[layer_idx]:
-                if type(graph[layer_idx]) == nn.BatchNorm2d and type(graph[bot_idx]) == QConv2d:
-                    conv_weight = graph[bot_idx].weight
-                    bn_weight = graph[layer_idx].weight
-                    bn_var = graph[layer_idx].running_var
-                    bn_eps = graph[layer_idx].eps
 
-                    graph[bot_idx].weight.copy_(conv_weight.clone().mul(bn_weight.view(-1, 1, 1, 1) / torch.sqrt(bn_var.view(-1, 1, 1, 1) + bn_eps)))
-                    
-                    if graph[bot_idx].bias is None:
-                        graph[bot_idx].bias = nn.Parameter(data=torch.zeros((graph[bot_idx].weight.size(0)), dtype=torch.float), requires_grad=False)
-
-                    conv_bias = graph[bot_idx].bias
-                    bn_bias = graph[layer_idx].bias
-                    bn_mean = graph[layer_idx].running_mean
-
-                    graph[bot_idx].bias.copy_(conv_bias.clone().mul(bn_weight.view(-1) / torch.sqrt(bn_var.view(-1) + bn_eps)).add(bn_bias.view(-1) -\
-						 (bn_weight.view(-1) * bn_mean.view(-1)) / torch.sqrt(bn_var.view(-1) + bn_eps)))
-
-                    graph[layer_idx].weight.mul_(0).add_(1)
-                    graph[layer_idx].running_var.mul_(0).add_(1)
-                    graph[layer_idx].bias.mul_(0)
-                    graph[layer_idx].running_mean.mul_(0)
-                    graph[layer_idx].eps = 0
-
-                    break
-
-    res = create_relation(graph, bottoms)
-
-    cross_layer_equalization(graph, res, visualize_state=False)
-
-    return model
+    model = merge_batchnorm(model, graph, bottoms)
+    torch.save(model.state_dict(), 'modeling/data_dependent_QuantConv2dAdd.pth')
 
 
 def inference_all(model):
     from utils.utils import forward_all
     args = lambda: 0
-    args.base_size = 512
-    args.crop_size = 512
+    args.base_size = 513
+    args.crop_size = 513
     voc_val = VOCSegmentation(args, split='val')
-    dataloader = DataLoader(voc_val, batch_size=5, shuffle=False, num_workers=0)
+    dataloader = DataLoader(voc_val, batch_size=32, shuffle=False, num_workers=0)
 
     forward_all(model, dataloader, visualize=False)
 
 
 def main():
+    data = torch.ones((4, 3, 513, 513))#.cuda()
+
+    # estimate_stats(data)
+    # return
+
     model = DeepLab(sync_bn=False)
     model.eval()
-    model = model.cuda()
+    
+    transformer = TorchTransformer()
 
-    # load trained parameters
-    state_dict = torch.load('modeling/deeplab-mobilenet.pth.tar')['state_dict']
-    model.load_state_dict(state_dict)
-    model = process_model(model)
+    model = switch_layers(model, transformer, data)
+
+    path_state_dict = 'modeling/data_dependent_QuantConv2dAdd.pth'
+    if os.path.exists(path_state_dict):
+        print("Load params from {}".format(path_state_dict))
+        state = torch.load(path_state_dict)
+ 
+        model.load_state_dict(state)
+
+    # use cpu to process
+    transformer = TorchTransformer()
+    model = model.cpu()
+    data = torch.ones((4, 3, 513, 513))#.cuda()
+    # transformer.summary(model, data)
+    # transformer.visualize(model, data, 'deeplab_graph', graph_size=120)
+    transformer._build_graph(model, data) # construt graph after all state_dict loaded
+
+    graph = transformer.log.getGraph()
+    bottoms = transformer.log.getBottoms()
+
+    # from PyTransformer.transformers.quantize import QuantMeasure
+    # def print_module(model):
+    #     for mm in model._modules:
+    #         if len(model._modules[mm]._modules) > 0 and\
+    #             not (len(model._modules[mm]._modules) == 1 and type(list(model._modules[mm]._modules.values())[0]) == QuantMeasure):
+    #             print_module(model._modules[mm])
+    #         else:
+    #             print(id(getattr(model, mm)), getattr(model, mm))
+
+    # print_module(model)
+
+    from PyTransformer.transformers.quantize import QuantConv2d
+    
+    model = merge_batchnorm(model, graph, bottoms, QuantConv2d)
+
+    #create relations
+    res = create_relation(graph, bottoms, QuantConv2d)
+    for ii in graph:
+        print(ii, graph[ii])
+    print('='*150)
+    for rr in res:
+        print(rr)
+    print('='*150)
+    return
+    cross_layer_equalization(graph, res, visualize_state=False)
+    
     model = model.cuda()
+    model.eval()
+
+    replace_op()
     inference_all(model)
-    # return
+    restore_op()
 
 
 if __name__ == '__main__':
